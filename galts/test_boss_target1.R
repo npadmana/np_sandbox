f4d2c64316e93ba6d8fbeb273cc6e212de152829
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-07-13 11:12:17 -0400

    Refactoring to do all galaxy types.

--- galts/test_boss_target1.R
+++ galts/test_boss_target1.R
@@ -5,21 +5,26 @@ source('~/myWork/nputils/R/BOSS/common.R')
 source('~/myWork/nputils/R/BOSS/galtarget.R')
 
 load("spall.xdr")
-flagval <- boss.Flagname2Val("GAL_CMASS", boss.target1)
-cmass1 <- spall[bitAnd(spall$TARGET1, flagval) > 0, ]
-cmass1 <- boss.ComputeAuxColors(cmass1)
-cmass1 <- boss.SelectCMASS(cmass1)
-small <- cmass1[is.finite(cmass1$is.CMASS), 
-                c("is.CMASS", "CHUNK", "PLUG_RA", "PLUG_DEC","PLATE", "FIBERID", "MJD")]
 
+test1 <- function (galtype) {
+  type1 <- boss.galTypes[[galtype]]
+  flagval <- boss.Flagname2Val(type1$name, boss.target1)
+  arr1 <- spall[bitAnd(spall$TARGET1, flagval) > 0, ]
+  arr1 <- boss.ComputeAuxColors(arr1)
+  arr1 <- type1$func(arr1)
+  col <- paste("is.",galtype,sep="")
+  names(arr1)[names(arr1) == col] <- "is.galtype"
+  arr1[!is.finite(arr1$is.galtype), "is.galtype"] <- FALSE 
+  
+  gg <- ggplot(arr1, aes(x=factor(CHUNK, 
+                                  levels=paste("boss", seq(1:30), sep=""), ordered=TRUE), 
+                         fill=factor(is.galtype, levels=c(TRUE, FALSE))))
+  gg <- gg + geom_bar(position="fill") + scale_x_discrete("Chunk") + 
+    scale_y_continuous("Fraction") + scale_fill_discrete(paste("Is",galtype,"?"))
+  myopts <- opts(axis.text.x=theme_text(angle=90.0))
+  print(gg + myopts)
+}
 
-gg <- ggplot(small, aes(x=factor(CHUNK, 
-                                 levels=paste("boss", seq(1:30), sep="")), 
-                        fill=factor(is.CMASS, levels=c(TRUE, FALSE))))
-gg <- gg + geom_bar(position="fill") + scale_x_discrete("Chunk") + 
-  scale_y_continuous("Fraction") + scale_fill_discrete("Is CMASS?")
-myopts <- opts(axis.text.x=theme_text(angle=90.0))
-print(gg + myopts)
 
 
 
