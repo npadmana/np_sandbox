15367333e0b226c0c2c5fdbbb93b81da59246c9a
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-07-13 00:16:49 -0400

    Generalize reading functions

--- galts/read_database.R
+++ galts/read_database.R
@@ -10,25 +10,32 @@ DBSetup <- function () {
 }
 
 
-Get.CMASS.SpAll <- function(conn) {
+Get.Sample.SpAll <- function(conn, galtype=c("GAL_CMASS")) {
   # Get a data frame of CMASS galaxies from spall
   # Currently hardcode things, but we will change this as necessary
+  colstr <- sprintf('("BOSS_TARGET1" & %i::bigint)::integer', 
+                    boss.Flagname2Val(galtype, boss.target1))
+  selectstr <- sprintf('%s as "TARGET1"',colstr)
+  wherestr <- sprintf('(%s > 0)', colstr)
   script <- paste('select "PLATE", "MJD", "FIBERID", "CHUNK", "Z_NOQSO", "ZWARNING_NOQSO", ',
                   '"PLUG_RA", "PLUG_DEC", "BOSS_TARGET1", ',
                   ' "CMODELFLUX_2", "CMODELFLUX_3", "CMODELFLUX_4",',
                   ' "MODELFLUX_2",  "MODELFLUX_3",  "MODELFLUX_4", ',
-                  ' "EXTINCTION_2", "EXTINCTION_3", "EXTINCTION_4" ',
+                  ' "EXTINCTION_2", "EXTINCTION_3", "EXTINCTION_4",',
+                  selectstr,
                   'from spall_v5_4_45_core natural join spall_v5_4_45_flux',
-                  'where ("SPECPRIMARY"=1)  AND (("BOSS_TARGET1" & 2::bigint) = 2)', sep=" ")
+                  'where ("SPECPRIMARY"=1)  AND',wherestr, sep=" ")
   return(dbGetQuery(conn, script))
 }
 
-MkCMASS1 <- function(fn) {
-  db1 <- DBSetup()
-  cmass1 <- Get.CMASS.SpAll(db1$conn)
-  save(cmass1, file=fn)
+Make.Sample.Spall <- function(fn) {
+  driv <- DBSetup()
+  tmp <- Get.Sample.SpAll(driv$conn, c("GAL_LOZ", "GAL_CMASS", "GAL_CMASS_SPARSE"))
+  save(tmp, file=fn)
+  dbDisconnect(driv$conn)
 }
 
-MkCMASS1("cmass1.xdr")
+Make.Sample.Spall("spall.xdr")
+
 
 
