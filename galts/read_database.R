f9bcd18aca92f94fb98e79b2b673d8476e4cd1ab
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-07-13 21:34:33 -0400

    switch to using a data table

--- galts/read_database.R
+++ galts/read_database.R
@@ -1,4 +1,5 @@
 library(RPostgreSQL)
+library(data.table)
 source('~/myWork/nputils/R/BOSS/common.R')
 source('~/myWork/nputils/R/BOSS/galtarget.R')
 
@@ -22,10 +23,12 @@ Get.Sample.SpAll <- function(conn, galtype=c("GAL_CMASS")) {
                   ' "CMODELFLUX_2", "CMODELFLUX_3", "CMODELFLUX_4",',
                   ' "MODELFLUX_2",  "MODELFLUX_3",  "MODELFLUX_4", ',
                   ' "EXTINCTION_2", "EXTINCTION_3", "EXTINCTION_4",',
+                  ' "PSFFLUX_3", "PSFFLUX_4", "PSFFLUX_5", "FIBER2FLUX_4", ',
+                  ' "SPECPRIMARY", ',
                   selectstr,
                   'from spall_v5_4_45_core natural join spall_v5_4_45_flux',
-                  'where ("SPECPRIMARY"=1)  AND',wherestr, sep=" ")
-  return(dbGetQuery(conn, script))
+                  'where ',wherestr, sep=" ")
+  return(data.table(dbGetQuery(conn, script)))
 }
 
 Make.Sample.Spall <- function(fn) {
