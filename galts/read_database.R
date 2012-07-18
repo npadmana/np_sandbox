1e88f81dc44d3e06aa977f38ceab62087e10bb7a
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-07-18 12:51:01 -0400

    Add in the z-band for some fluxes.

--- galts/read_database.R
+++ galts/read_database.R
@@ -20,9 +20,9 @@ Get.Sample.SpAll <- function(conn, galtype=c("GAL_CMASS")) {
   wherestr <- sprintf('(%s > 0)', colstr)
   script <- paste('select "PLATE", "MJD", "FIBERID", "CHUNK", "Z_NOQSO", "ZWARNING_NOQSO", ',
                   '"PLUG_RA", "PLUG_DEC", "BOSS_TARGET1", ',
-                  ' "CMODELFLUX_2", "CMODELFLUX_3", "CMODELFLUX_4",',
-                  ' "MODELFLUX_2",  "MODELFLUX_3",  "MODELFLUX_4", ',
-                  ' "EXTINCTION_2", "EXTINCTION_3", "EXTINCTION_4",',
+                  ' "CMODELFLUX_2", "CMODELFLUX_3", "CMODELFLUX_4", "CMODELFLUX_5", ',
+                  ' "MODELFLUX_2",  "MODELFLUX_3",  "MODELFLUX_4", "MODELFLUX_5", ',
+                  ' "EXTINCTION_2", "EXTINCTION_3", "EXTINCTION_4", "EXTINCTION_5", ',
                   ' "PSFFLUX_3", "PSFFLUX_4", "PSFFLUX_5", "FIBER2FLUX_4", ',
                   ' "SPECPRIMARY", ',
                   selectstr,
